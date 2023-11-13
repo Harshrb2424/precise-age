7bd914a2454e85df42e5e5ea469fd03dbac2bef3
Harsh RB <harshrb2424@gmail.com>
2023-11-13 22:48:27 +0530

    animation, bug fix

--- index.js
+++ index.js
@@ -4,27 +4,18 @@ document.addEventListener('DOMContentLoaded', function () {
 
   var birthdayInput = document.getElementById("birthday");
   var hbdLink = document.getElementById("hbd");
-
-  // Add a click event listener to the link
   hbdLink.addEventListener("click", function(event) {
-    // Prevent the default behavior of the link
     event.preventDefault();
-
-    // Get the selected date from the input
     var selectedDate = birthdayInput.value;
     console.log(birthdayInput.value);
-    // Parse the selected date to get year, month, and date
     var dateObject = new Date(selectedDate);
     var year = dateObject.getFullYear();
-    var month = dateObject.getMonth() + 1; // Months are zero-indexed
+    var month = dateObject.getMonth() + 1; 
     var day = dateObject.getDate();
-
-    // Update the href with the new parameters
     hbdLink.href = "index.html?birthYear=" + year + "&birthMonth=" + month + "&birthDate=" + day;
     console.log(hbdLink.href);
     window.location.href = hbdLink.href;
   });
-
 
   const animatedValueElement = document.getElementById('animatedValue');
 
@@ -44,13 +35,10 @@ document.addEventListener('DOMContentLoaded', function () {
 
     const birthday = `${year}-${month}-${date}`;
     const yearValue = 1000 * 60 * 60 * 24 * 365.25;
-          // Divide Time with a year
           const bd = new Date(birthday);
           let byears = (bd.getTime() / yearValue);
-          // console.log(byears);
           const d = new Date();
           let years = (d.getTime() / yearValue);
-          // console.log(years);
           let age = years - byears;
           console.log(age.toFixed(9));
           if (year == 1000) {
@@ -81,18 +69,17 @@ document.addEventListener('DOMContentLoaded', function () {
 
   setInterval(updateFromUrl, 1000);
 
-  updateFromUrl(); // Initial update
+  updateFromUrl();
 });
 
-
-//* file:///E:/Harsh%20%7BLaptop%7D/Backup%2004%202023/Skills/Code/Exercise/2022-23/Web%20Dev%20V2/Projects/decimal-age/index.html?birthYear=2004&birthMonth=11&birthDate=5
-//* file:///E:/Harsh%20%7BLaptop%7D/Backup%2004%202023/Skills/Code/Exercise/2022-23/Web%20Dev%20V2/Projects/decimal-age/index.html
 $(document).ready(function() {
-
-
   $("form").fadeIn();
+  console.log($("h2").text());
+  if ($("h2").text() == "Your Age is") {
+    $("form").hide();
+  }
 
   setTimeout(function() {
     $("form").fadeOut();
-  }, 5000); // 5000 milliseconds = 5 seconds
+  }, 30000);
 });
